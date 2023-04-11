ebe038373fbfe38222d7e3a4992ffeff898ecd35
Jeremias <jeremiaspion@gmail.com>
2023-04-11 14:54:23 -0300

    Add links in model home

--- home/admin.py
+++ home/admin.py
@@ -7,8 +7,13 @@ from django.utils.html import format_html
 
 
 class HomeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name_city', 'banner_photo_city')
+    list_display = ('id', 'name_city', 'banner_photo_city', 'edit_city_link')
     list_display_links = ('name_city',)
+
+    def edit_city_link(self, obj):
+         url = reverse('admin:cities_city_change', args=[obj.city.pk])
+         return format_html('<a href="{}"> Editar cidade</a>', url)
+    edit_city_link.short_description = 'Editar Cidade'
 
 
 admin.site.register(Home, HomeAdmin)
