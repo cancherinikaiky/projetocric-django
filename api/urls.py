77689fdadf3697c08eede792f8586f766c91f8a7
Jeremias <jeremiaspion@gmail.com>
2023-04-18 11:07:53 -0300

    Add url home_cities

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from .views import CityList, CityDetail, HomeRoutes
 urlpatterns = [
     path('cities/', CityList.as_view()),
     path('cities/<int:pk>/', CityDetail.as_view()),
-    path('home_routes/', HomeRoutes.as_view()),
+    path('home_cities/', HomeRoutes.as_view()),
 ]
