83884d761ace090857e1c6a26fc6a546d0738068
Jeremias Piontkoski <jeremiaspion@gmail.com>
2023-05-02 12:14:54 -0300

    Add multiple upload images in event

--- event/models/event.py
+++ event/models/event.py
@@ -3,7 +3,7 @@ from django.db import models
 from cities.models import Route
 
 class Event(models.Model):
-    description = models.CharField(max_length=500, verbose_name="Descrição do evento")
+    description = models.CharField(max_length=500, verbose_name="Descrição do evento", null=True)
     routes = models.ManyToManyField(Route, blank=True, null=True, verbose_name="Rotas do Evento")
 
     def __str__(self) -> str:
