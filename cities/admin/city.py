f1eb00ac4cd277e36e115edcaf7bb47e1cce3b23
Jeremias Piontkoski <jeremiaspion@gmail.com>
2023-05-02 08:50:53 -0300

    Add replace name images

--- cities/admin/city.py
+++ cities/admin/city.py
@@ -7,6 +7,7 @@ class CityAdmin(admin.ModelAdmin):
     form = CityAdminForm
     list_display = ('id', 'name', 'visible')
     list_display_links = ('id', 'name')
+    list
 
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         if db_field.name == "routes":
--- cities/signals.py
+++ cities/signals.py
@@ -2,9 +2,9 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
 from .models.api_strava import Api
-from unidecode import unidecode
 
-from .models import Route
+from .models import Route, City, Category
+from projetocric.utilities import replace
 
 @receiver(pre_save, sender=Route)
 def insert_polyline(sender, instance, **kwargs):
@@ -15,5 +15,13 @@ def insert_polyline(sender, instance, **kwargs):
     except KeyError:
         instance.polyline = None
 
-def replace(name):
-    return unidecode(name).strip().lower().replace(' ', '_').replace('-', '_')
+@receiver(pre_save, sender=City)
+def change_name_banner_image(sender, instance, **kwargs):
+    if instance.banner_image:   
+       instance.banner_image.name = replace(instance.banner_image.name)
+
+@receiver(pre_save, sender=Category)
+def change_name_category_image(sender, instance, **kwargs):
+    if instance.image:
+        instance.image.name = replace(instance.image.name)
+
