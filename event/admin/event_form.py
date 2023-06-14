d0e473e3d982fa7b353fc5938b1b8117bc33779f
Seu Nome <3augustto3@gmail.com>
2023-06-14 16:14:21 -0300

    Admin page based on event added

--- event/admin/event_form.py
+++ event/admin/event_form.py
@@ -1,5 +1,6 @@
 from django import forms
 from event.models import Event
+from django.contrib.auth.models import User
 
 
 class EventForm(forms.ModelForm):
@@ -13,3 +14,8 @@ class EventForm(forms.ModelForm):
             'warnings': forms.CheckboxSelectMultiple,
         }
         
+    users = forms.ModelMultipleChoiceField(
+        queryset=User.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
